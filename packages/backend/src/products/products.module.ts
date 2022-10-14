d1693e1cfdb92a2f5cf595b274b2919f7c547372
PatrikVahlen <91195238+PatrikVahlen@users.noreply.github.com>
2022-10-14 15:36:51 +0200

    Multer for fun

--- packages/backend/src/products/products.module.ts
+++ packages/backend/src/products/products.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
 import { ProductsController } from './products.controller';
 import { Products, ProductsSchema } from './products.schema';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Products.name, schema: ProductsSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Products.name, schema: ProductsSchema }]),
+    MulterModule.register({
+      dest: './files',
+    })],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
 })
-export class ProductsModule {}
+export class ProductsModule { }
