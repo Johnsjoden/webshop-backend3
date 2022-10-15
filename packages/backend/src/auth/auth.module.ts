867e7d0e6990e7a74be150ff3a95868217b08c3f
Johnsjoden <89608491+Johnsjoden@users.noreply.github.com>
2022-10-15 15:05:39 +0200

    Update auth.module.ts

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -1,23 +1,24 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.modul';
-import { ProductsModule } from './products/products.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { ConfigModule } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { UserService } from 'src/user/user.service';
+import { UserModule } from 'src/user/user.modul';
+import { AuthController } from './auth.controller';
+
 @Module({
-  imports: [
-    UserModule,
-    AuthModule,
-    ProductsModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '/files'),
-    }),
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URL),
-  ],
-  controllers: [],
-  providers: []
+    imports: [
+        UserModule,
+        PassportModule,
+        JwtModule.register({
+            secret: jwtConstants.secret,
+            signOptions: { expiresIn: '60s' },
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, LocalStrategy, JwtService],
+    exports: [AuthService, LocalStrategy],
 })
-export class AppModule { }
+export class AuthModule { }
