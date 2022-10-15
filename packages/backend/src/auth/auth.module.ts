0898adcb3f049a490b103ff5e1f376a23265fb7d
Johnsjoden <89608491+Johnsjoden@users.noreply.github.com>
2022-10-15 15:04:54 +0200

    Update auth.module.ts

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -1,1 +1,23 @@
-
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.modul';
+import { ProductsModule } from './products/products.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+@Module({
+  imports: [
+    UserModule,
+    AuthModule,
+    ProductsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '/files'),
+    }),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+  ],
+  controllers: [],
+  providers: []
+})
+export class AppModule { }
