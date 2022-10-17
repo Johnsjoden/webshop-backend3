76a7e23aa435fb2c678d600051cbef08997fb913
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-10-17 10:41:06 +0200

    Add new route + registerpage - WIP

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import StartPage from './pages/StartPage';
 import DetailPage from './pages/DetailPage';
 import LogInPage from './pages/LogInPage';
 import UserPage from './pages/UserPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <header>
         <Routes>
           <Route path="/" element={<StartPage />} />
+          <Route path="/register" element={<RegisterPage/>}></Route>
           <Route path="/detail" element={<DetailPage />} />
           <Route path="/user/login" element={<LogInPage />} />
           <Route path="/user/userinfo" element={<UserPage />} />
