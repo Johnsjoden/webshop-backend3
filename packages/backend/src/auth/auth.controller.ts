504f26b82ccbc4f306d6e28a4cbbc5965a1602cc
john sjödén <johnsjoden95@gmail.com>
2022-10-17 14:50:34 +0200

    Added some routes to users to handle orders

--- packages/backend/src/auth/auth.controller.ts
+++ packages/backend/src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth-guard';
 
 @Controller("auth")
 export class AuthController {
-    constructor(private readonly authService: AuthService){}
+    constructor(private readonly authService: AuthService) { }
     @UseGuards(AuthGuard('local'))
     @Post('/login')
     async login(@Request() req) {
