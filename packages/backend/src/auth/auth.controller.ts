d2259e735977f978c21abfd2d2bad35f5682db26
PatrikVahlen <91195238+PatrikVahlen@users.noreply.github.com>
2022-10-17 14:47:14 +0200

    JWT-Strategy

--- packages/backend/src/auth/auth.controller.ts
+++ packages/backend/src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-authguard';
 
 @Controller("auth")
 export class AuthController {
-    constructor(private readonly authService: AuthService){}
+    constructor(private readonly authService: AuthService) { }
     @UseGuards(AuthGuard('local'))
     @Post('/login')
     async login(@Request() req) {
         return this.authService.login(req.user)
+    }
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
