b2276ddef686e1f9931acfa6135f70cfbaa8614b
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-10-18 18:39:31 +0200

    WIP update to basket

--- packages/shared/src/product-items.ts
+++ packages/shared/src/product-items.ts
@@ -1,9 +1,9 @@
 export default interface ProductItems {
     description: string,
     title: string,
-    image_url: string,
     category: string,
     weight: string,
     price: string,
     manufacturer: string,
+    image_url?: string,
 }
